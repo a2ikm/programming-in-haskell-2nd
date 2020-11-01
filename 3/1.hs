c2d071b9724b25a9c3c89ab648185124d8bf5b12
Masato Ikeda <masato.ikeda@gmail.com>
2020-11-01 17:53:53 +0900

    Finished Chapter 3

---
+++ 3/1.hs
@@ -0,0 +1,9 @@
+['a', 'b', 'c'] :: [Char]
+
+('a', 'b', 'c') :: (Char, Char, Char)
+
+[(False, '0'), (True, '1')] :: [(Boolean, Char)]
+
+([False, True], ['0', '1']) :: ([Boolean], [Char])
+
+[tail, init, reverse] :: [[a] -> [a]]
---
+++ 3/3.hs
@@ -0,0 +1,27 @@
+second xs = head (tail xs)
+second :: [a] -> a
+
+--
+
+swap (x, y) = (y, x)
+swap :: (a, b) -> (b, a)
+
+--
+
+pair x y = (x, y)
+pair :: a -> b -> (a, b)
+
+--
+
+double x = x * 2
+double :: Num => a -> a -> a
+
+--
+
+palindrome xs = reverse xs == xs
+palindrome :: [a] -> Boolean
+
+--
+
+twice f x = f (f x)
+twice :: (a -> a) -> a -> a
